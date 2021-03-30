d745e1f0617a16b694476ebe8fad424887c62ad9
skydoves <skydoves@naver.com>
2021-03-31 01:59:42 +0900

    Implement whatIfFindFragment by tag name

--- whatif-android/src/main/java/com/skydoves/whatif_android/WhatIfFragmentActivity.kt
+++ whatif-android/src/main/java/com/skydoves/whatif_android/WhatIfFragmentActivity.kt
@@ -57,3 +57,40 @@ inline fun <reified T : Fragment> FragmentActivity.whatIfFindFragment(
 ) {
   supportFragmentManager.findFragmentById(id).whatIfNotNullAs(whatIf, whatIfNot)
 }
+
+/**
+ * @author skydoves (Jaewoong Eum)
+ *
+ * An expression for invoking [whatIf] when the [FragmentActivity] has an attached fragment [T].
+ *
+ * @param id A fragment currently on the back stack associated with this tag name.
+ * @param whatIf An executable lambda function if the [FragmentActivity] has a fragment [T].
+ */
+@JvmSynthetic
+@WhatIfInlineOnly
+inline fun <reified T : Fragment> FragmentActivity.whatIfFindFragment(
+  tag: String?,
+  whatIf: (T) -> Unit
+) {
+  whatIfFindFragment(tag, whatIf, {})
+}
+
+/**
+ * @author skydoves (Jaewoong Eum)
+ *
+ * An expression for invoking [whatIf] when the [FragmentActivity] has an attached fragment [T].
+ * If [FragmentActivity] has not an attached fragment [T], [whatIfNot] will be invoked instead of the [whatIf].
+ *
+ * @param tag A fragment currently on the back stack associated with this tag name.
+ * @param whatIf An executable lambda function if the [FragmentActivity] has a fragment [T].
+ * @param whatIfNot An executable lambda function if the [FragmentActivity] has not an attached fragment [T].
+ */
+@JvmSynthetic
+@WhatIfInlineOnly
+inline fun <reified T : Fragment> FragmentActivity.whatIfFindFragment(
+  tag: String?,
+  whatIf: (T) -> Unit,
+  whatIfNot: () -> Unit
+) {
+  supportFragmentManager.findFragmentByTag(tag).whatIfNotNullAs(whatIf, whatIfNot)
+}
