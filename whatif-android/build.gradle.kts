154640d4fd00278f92978c7d07c57cde2c4a46f1
skydoves <skydoves@naver.com>
2023-10-08 16:46:05 +0900

    Update jvm target to 11

--- whatif-android/build.gradle.kts
+++ whatif-android/build.gradle.kts
@@ -29,6 +29,11 @@ android {
     minSdk = Configuration.minSdk
   }
 
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+  }
+
   kotlinOptions {
     jvmTarget = libs.versions.jvmTarget.get()
   }
